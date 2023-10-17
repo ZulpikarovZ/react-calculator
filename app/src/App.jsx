ecec0c5e986508cd0a16d1c2254b73b56a76eed9
Zaur <Zulpikarov.Z@gmail.com>
2023-10-17 19:14:44 +0300

    calc v0.2

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,40 +1,105 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import styles from './App.module.css';
 
 export const App = () => {
 	const buttons = ['C', '←', '%', '/', 7, 8, 9, '*', 4, 5, 6, '-', 1, 2, 3, '+', '☻', 0, '☺', '='];
-	const isWhite = (el) => (typeof el === 'number' ? styles.white : styles.orange);
+	const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+	const operations = ['+', '-', '*', '/'];
 
-	const [view, setView] = useState(0);
-	const [result, setResult] = useState(false);
+	const [orange, setOrange] = useState(false);
+	const [state, setState] = useState({
+		view: '0',
+		first: '',
+		second: '',
+		sign: '',
+	});
 
-	const numClick = (e) => {
-		if (e.target.innerText === '=') {
-			try {
-				setResult(true);
-				setView(eval(view));
-			} catch (error) {
-				setResult(false);
-				setView('Неверный ввод! ');
-				setTimeout(() => {
-					setView(0);
-				}, 1500);
-			}
-		} else if (e.target.innerText === 'C') {
-			setResult(false);
-			setView(0);
+	const { view, first, second, sign } = state;
+
+	const handleNumberClick = (btn) => {
+		setOrange(false);
+
+		if (!sign) {
+			const newFirst = first === '0' ? btn : first + btn;
+			setState({ ...state, first: newFirst, view: newFirst });
 		} else {
-			setResult(false);
-			setView((prev) => (prev === 0 ? e.target.innerText : prev + e.target.innerText));
+			const newSecond = second === '0' ? btn : second + btn;
+			setState({ ...state, second: newSecond, view: newSecond });
 		}
 	};
+
+	const handleOperationClick = (btn) => {
+		setOrange(false);
+
+		setState({ ...state, sign: btn, view: btn });
+	};
+
+	const handleEqualClick = () => {
+		setOrange(true);
+
+		let result;
+		const numFirst = parseFloat(first);
+		const numSecond = parseFloat(second);
+
+		switch (sign) {
+			case '+':
+				result = numFirst + numSecond;
+				break;
+			case '-':
+				result = numFirst - numSecond;
+				break;
+			case '*':
+				result = numFirst * numSecond;
+				break;
+			case '/':
+				result = numFirst / numSecond;
+				break;
+			default:
+				return;
+		}
+
+		const newView = String(result);
+		setState({ view: newView, first: newView, second: '', sign: '' });
+	};
+
+	const handleClearClick = () => {
+		setOrange(false);
+		setState({ view: '0', first: '', second: '', sign: '' });
+	};
+
+	const handleButtonClick = (btn) => {
+		if (numbers.includes(btn)) {
+			handleNumberClick(btn);
+			return;
+		}
+
+		if (operations.includes(btn)) {
+			handleOperationClick(btn);
+			return;
+		}
+
+		if (btn === '=') {
+			handleEqualClick();
+			return;
+		}
+
+		if (btn === 'C') {
+			handleClearClick();
+		}
+	};
+
+	const isWhite = (el) => (typeof el === 'number' ? styles.white : styles.orange);
 
 	return (
 		<div className={styles.app}>
 			<div className={styles.calc}>
-				<div className={styles.header + ' ' + (result ? styles.orange : styles.white)}>{view}</div>
+				<div className={styles.header + ' ' + (orange ? styles.orange : styles.white)}>{view}</div>
 				{buttons.map((el) => (
-					<div className={styles.ceil + ' ' + isWhite(el)} key={el} onClick={(e) => numClick(e)}>
+					<div
+						className={`${styles.ceil} ${isWhite(el)}`}
+						key={el}
+						onClick={() => handleButtonClick(String(el))}
+					>
 						{el}
 					</div>
 				))}
